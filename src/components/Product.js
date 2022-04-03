d4e08b2503536ee1df3d84ca2553908a29be1996
Bogdan Seliman <selimanbogdan@gmail.com>
2022-04-03 18:27:34 +0300

    Loading-skeleton and FilterProduct

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
+import Skeleton from 'react-loading-skeleton';
 
 export default function Product() {
 
@@ -32,19 +33,37 @@ export default function Product() {
     const Loading = () => {
         return (
             <>
-                Loading....
+                <div className='col-md-3'>
+                    <Skeleton height={350} />
+                </div>
+                <div className='col-md-3'>
+                    <Skeleton height={350} />
+                </div>
+                <div className='col-md-3'>
+                    <Skeleton height={350} />
+                </div>
+                <div className='col-md-3'>
+                    <Skeleton height={350} />
+                </div>
             </>
         )
     }
+    const filterProduct = (cat) => {
+        const updatedList = data.filter((x) => x.category === cat);
+        setFilter(updatedList);
+        console.log(cat)
+        //console.log(updatedList)
+    }
+
     const ShowProducts = () => {
         return (
             <>
                 <div className='buttons d-flex justify-content-center mb-5 pb-5'>
-                    <button className='btn btn-outline-dark me-2 '>All</button>
-                    <button className='btn btn-outline-dark me-2'>Men's Clothing</button>
-                    <button className='btn btn-outline-dark me-2'>Women's Clothing</button>
-                    <button className='btn btn-outline-dark me-2'>Jewlery Clothing</button>
-                    <button className='btn btn-outline-dark me-2'>Electronic</button>
+                    <button className='btn btn-outline-dark me-2' onClick={() => setFilter(data)}>All</button>
+                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("men's clothing")} >Men's Clothing</button>
+                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
+                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("jewlery clothing")}>Jewlery Clothing</button>
+                    <button className='btn btn-outline-dark me-2' onClick={() => filterProduct("electronics")}>Electronic</button>
                 </div>
 
                 {
@@ -55,11 +74,11 @@ export default function Product() {
                                     <div className='card h-100 text-center p-4' key={product.id}>
                                         <img src={product.image} className='card-img-top' alt={product.title} height='250px' />
                                         <div className='card-body' >
-                                            <h5 className='card-title mb-0'> {product.title.substring(0, 12)}</h5>
-                                            <p className='card-text'>
+                                            <h5 className='card-title mb-0'> {product.title.substring(0, 12)}...</h5>
+                                            <p className='card-text lead fw-bold'>
                                                 ${product.price}
                                             </p>
-                                            <a href='' className='btn btn-primary' >Buy Now</a>
+                                            <a href='' className='btn btn-outline-dark' >Buy Now</a>
                                         </div>
                                     </div>
                                 </div>
