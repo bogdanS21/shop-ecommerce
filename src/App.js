95617e028d02f0711c53263df02187e40de473b6
Bogdan Seliman <selimanbogdan@gmail.com>
2022-04-03 16:40:34 +0300

    Product page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Product from './components/Product';
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      <Product />
     </>
   );
 }
